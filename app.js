b0ea3f03861d93ea338c2462c1418c8d06c1616f
GarauOR <luca91garau@gmail.com>
2022-10-29 11:44:43 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//BURGER MENU START
+const burgerMenu = document.querySelector("#burgerMenu");
+const menu = document.querySelector(".menu");
+
+burgerMenu.addEventListener("click", () => {
+    menu.classList.toggle("toggleMenu");
+})
+//BURGER MENU END
+
