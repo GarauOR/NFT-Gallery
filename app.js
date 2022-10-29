155c9f520683b5d11060a5d0ac6b2eea28b6ae88
GarauOR <luca91garau@gmail.com>
2022-10-29 16:28:20 +0100

    adding slider

--- app.js
+++ app.js
@@ -4,6 +4,42 @@ const menu = document.querySelector(".menu");
 
 burgerMenu.addEventListener("click", () => {
     menu.classList.toggle("toggleMenu");
-})
+});
 //BURGER MENU END
 
+/* SLIDER START */
+const rightSlide = document.querySelector("#rightSlide");
+const leftSlide = document.querySelector("#leftSlide");
+let imgArr = ["img/nft1.bmp", "img/nft2.bmp", "img/nft3.bmp"];
+let descrArr = ["Beautiful, passionate Red!", "Intense, dreaming Blue", "Wild freedom, Green"];
+let priceArr = [10, 0.578, 2.32];
+
+const img = document.querySelector("#slider img");
+const descr = document.querySelector("#descr p");
+const price = document.querySelector("#price");
+let i=0;
+img.src = imgArr[i];
+descr.textContent = descrArr[i];
+price.textContent = `${priceArr[i]} Eth`;
+
+rightSlide.addEventListener("click", () => {
+    if(i<(imgArr.length-1)){
+        i++;
+        img.src = imgArr[i];
+        descr.textContent = descrArr[i];
+        price.textContent = priceArr[i];
+    }
+});
+
+leftSlide.addEventListener("click", () => {
+    if(i>0){
+        i--;
+        img.src = imgArr[i];
+        descr.textContent = descrArr[i];
+        price.textContent = priceArr[i];
+    }
+});
+/* SLIDER END */
+
+/* SESSION STORAGE START */
+/* SESSION STORAGE END */
