8a5dd3ff3dbf8b8157140553b51c1529493578cb
GarauOR <luca91garau@gmail.com>
2022-11-03 11:08:47 +0000

    website brain completed and code cleaned, missing the style

--- app.js
+++ app.js
@@ -7,36 +7,55 @@ burgerMenu.addEventListener("click", () => {
 });
 //BURGER MENU END
 
+/* INITIALIZING NFTARRAY START */
+let nftArr = [{
+    image: "img/nft1.bmp",
+    descr: "Beautiful, passionate Red!",
+    price: 10,
+    name: "Red storm",
+    artist: "Luca Garau",
+    }, {
+    image: "img/nft2.bmp",
+    descr: "Intense, dreaming Blue",
+    price: 0.578,
+    name: "Blue Tempest",
+    artist: "Luca Garau",
+    }, {
+    image: "img/nft3.bmp",
+    descr: "Wild freedom, Green",
+    price: 2.32,   
+    name: "Green Spark",
+    artist: "Luca Garau", 
+    }];
+/* INITIALIZING NFTARRAY END */
+
 /* SLIDER START */
 const rightSlide = document.querySelector("#rightSlide");
 const leftSlide = document.querySelector("#leftSlide");
-let imgArr = ["img/nft1.bmp", "img/nft2.bmp", "img/nft3.bmp"];
-let descrArr = ["Beautiful, passionate Red!", "Intense, dreaming Blue", "Wild freedom, Green"];
-let priceArr = [10, 0.578, 2.32];
-
 const img = document.querySelector("#slider img");
 const descr = document.querySelector("#descr p");
 const price = document.querySelector("#price");
+
+let sliding = (i) => {
+    img.src = nftArr[i].image;
+    descr.textContent = nftArr[i].descr;
+    price.textContent = `${nftArr[i].price} Eth`;
+};
+
 let i=0;
-img.src = imgArr[i];
-descr.textContent = descrArr[i];
-price.textContent = `${priceArr[i]} Eth`;
+sliding(i);
 
 rightSlide.addEventListener("click", () => {
-    if(i<(imgArr.length-1)){
+    if(i<(nftArr.length-1)){
         i++;
-        img.src = imgArr[i];
-        descr.textContent = descrArr[i];
-        price.textContent = `${priceArr[i]} Eth`;
+        sliding(i);
     }
 });
 
 leftSlide.addEventListener("click", () => {
     if(i>0){
         i--;
-        img.src = imgArr[i];
-        descr.textContent = descrArr[i];
-        price.textContent = `${priceArr[i]} Eth`;
+        sliding(i);
     }
 });
 /* SLIDER END */
@@ -53,25 +72,97 @@ counter.textContent = countIdx;
 /* ITEM COUNTER END */
 
 /* LOCAL STORAGE START */
-let nameArr = ["Red storm", "Blue Tempest", "Green Spark"];
-let artistArr = ["Luca Garau", "Luca Garau", "Luca Garau"];
 let cartArr = [];
 const addCart = document.querySelector("#descr button");
+
 addCart.addEventListener("click", () => {
     if(localStorage["shopping"]){
         let retrieving = localStorage["shopping"];
         cartArr = JSON.parse(retrieving);
     }
-    let itemCart = {
-        image: imgArr[i],
-        name: nameArr[i],
-        artist: artistArr[i],
-        price: priceArr[i]
+
+    //UNIQUE ITEMS START
+    let unique = (objArr) => {
+        let j = 0;
+        objArr.forEach((item) => {
+            if(item.name == nftArr[i].name){
+                j++;
+            }
+        });
+        return j
     };
-    cartArr.push(itemCart);
+    console.log(unique(cartArr));
+    if (unique(cartArr)<1){
+        cartArr.push(nftArr[i]);
+        countIdx += 1;
+    }
+    //UNIQUE ITEMS END
     let stringifiedArr = JSON.stringify(cartArr);
-    localStorage.setItem("shopping",stringifiedArr);
-    countIdx += 1;
+    localStorage.setItem("shopping",stringifiedArr);    
     counter.textContent = countIdx;
 });
 /* LOCAL STORAGE END */
+
+/* CONVERTER START */
+let pounds = document.querySelector("#pounds");
+let eths = document.querySelector("#ethereum");
+
+pounds.addEventListener("keyup", () => {
+    if(pounds.value>0){
+        eths.value = pounds.value / 1363.29;
+    }
+    else {eths.value = "none"}
+});
+
+eths.addEventListener("keyup", () => {
+    if(eths.value>0){
+        pounds.value = eths.value * 1363.29;
+    }
+    else {pounds.value = "none"}
+});
+/* CONVERTER END */
+
+/* NEWSFEED START */
+//DECLARING
+let newsArr = [
+    {image: "https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/H8WuRINimqur8ud/finance-abstract-financial-background_sr2reankl_thumbnail-1080_01.png",
+    para: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil minima eos similique adipisci recusandae? Deserunt, voluptas! Saepe provident reprehenderit eius itaque, quis totam, molestiae culpa sit earum distinctio at aperiam?",
+    title: "Title 1",
+    dot: document.querySelector("#dot1"),},
+    {image: "https://th.bing.com/th/id/R.e8e94cfb7eb1113e0b3333f25192b098?rik=fqTHOJ%2br2OiBAw&pid=ImgRaw&r=0",
+    para: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil minima eos similique adipisci recusandae? Deserunt, voluptas! Saepe provident reprehenderit eius itaque, quis totam, molestiae culpa sit earum distinctio at aperiam?",
+    title: "Title 2",
+    dot: document.querySelector("#dot2"),},
+    {image: "https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/H8WuRINimqur8ud/global-economy-finance-background_b86pzpyq__d_thumbnail-1080_01.png",
+    para: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil minima eos similique adipisci recusandae? Deserunt, voluptas! Saepe provident reprehenderit eius itaque, quis totam, molestiae culpa sit earum distinctio at aperiam?",
+    title: "Title 3",
+    dot: document.querySelector("#dot3"),}
+];
+const imgNews = document.querySelector("#newsBox img");
+const paraNews = document.querySelector("#newsBox p");
+const titleNews = document.querySelector("#newsBox h3");
+let newsValue = () => {
+    imgNews.src = newsArr[idx].image;
+    titleNews.textContent = newsArr[idx].title;
+    paraNews.textContent = newsArr[idx].para;
+    newsArr[idx].dot.style.backgroundColor = ("white");
+    idx++;
+};
+let idx = 0;
+
+newsValue();
+
+//AUTOMATED SLIDE NEWS
+let assignNews = () => {
+    if(idx==newsArr.length){
+        idx=0
+        newsArr[newsArr.length-1].dot.style.backgroundColor = ("transparent");
+        newsValue();
+    }
+    else{
+    newsArr[idx-1].dot.style.backgroundColor = ("transparent");
+    newsValue();
+    }
+};  
+setInterval(assignNews, 8000);
+/* NEWSFEED END */
--- appCart.js
+++ appCart.js
@@ -30,40 +30,44 @@ const listCont = document.querySelector("#listCont");
 const empty = document.querySelector("#empty");
 let tot = 0;
 let deleteArr = [];
-if(0<cartArr.length){
+
+
+if(cartArr.length>0){
     empty.style.display = "none";
     cartArr.forEach((item) => {
-        //DIV
+        //INITIALISING
         const listItem = document.createElement("div");
+        const itemImg = document.createElement("img");
+        const itemName = document.createElement("h3");
+        const PnDCont = document.createElement("div");
+        const itemPrice = document.createElement("h3");
+        const itemDelete = document.createElement("button");
+
+        //DIV
         listItem.classList.add("listItem");
         listItem.setAttribute("id", item.id);
         listCont.appendChild(listItem);
         deleteArr.push(item.id);
 
         //img
-        const itemImg = document.createElement("img");
         itemImg.src = item.image;
         itemImg.classList.add("itemImg");
         listItem.appendChild(itemImg);
 
         //name
-        const itemName = document.createElement("h3");
         itemName.textContent = `The ${item.name} by ${item.artist}`;
         itemName.classList.add("itemName");
         listItem.appendChild(itemName);
 
         //price&delete
         //cont
-        const PnDCont = document.createElement("div");
         PnDCont.classList.add("PnDCont");
         listItem.appendChild(PnDCont);
         //price
-        const itemPrice = document.createElement("h3");
         itemPrice.textContent = `${item.price} Eth`;
         itemPrice.classList.add("itemPrice");
         PnDCont.appendChild(itemPrice);
         //delete
-        const itemDelete = document.createElement("button");
         itemDelete.textContent = "Remove";
         itemDelete.classList.add("itemDelete");
         itemDelete.setAttribute("id", item.id);
@@ -73,7 +77,7 @@ if(0<cartArr.length){
     });
 }
 else {
-    empty.style.display = "block";
+    empty.style.display = "block"; 
 }
 /* LIST BUILDING END */
 
@@ -112,7 +116,7 @@ if(0<cartArr.length){
         
 /* REMOVE ALL START */
 let cancelAll = document.querySelector(".checkOut");
-if(0<cartArr.length){
+if(cartArr.length>0){
 cancelAll.addEventListener("click", () => {
     localStorage.clear();
     window.location = "cart.html";
@@ -123,7 +127,7 @@ cancelAll.addEventListener("click", () => {
 /* REMOVE ITEM START */
 let butRefArr = [];
 let stringifiedArr;
-if(0<cartArr.length){
+if(cartArr.length>0){
     deleteArr.forEach((item) => {
         butRefArr.push(document.querySelector(`#${item}`).lastChild);
 });
@@ -135,7 +139,7 @@ deleteArr.forEach((item, i) => {
         cartArr.splice(i, 1);
         butRefArr.splice(i, 1);
         deleteArr.splice(i, 1);
-        if(cartArr.length){
+        if(cartArr.length>=0){
             let countIdx = cartArr.length;
             localStorage.setItem("index", countIdx);
         }
@@ -149,5 +153,65 @@ deleteArr.forEach((item, i) => {
 }
 /* REMOVE ITEM END */
 
-/* DATA START */
-/* DATA END */
+/* CONVERTER START */
+let pounds = document.querySelector("#pounds");
+let eths = document.querySelector("#ethereum");
+
+pounds.addEventListener("keyup", () => {
+    if(pounds.value>0){
+        eths.value = pounds.value / 1363.29;
+    }
+    else {eths.value = "none"}
+});
+
+eths.addEventListener("keyup", () => {
+    if(eths.value>0){
+        pounds.value = eths.value * 1363.29;
+    }
+    else {pounds.value = "none"}
+});
+/* CONVERTER END */
+
+/* NEWSFEED START */
+//DECLARING
+let newsArr = [
+    {image: "https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/H8WuRINimqur8ud/finance-abstract-financial-background_sr2reankl_thumbnail-1080_01.png",
+    para: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil minima eos similique adipisci recusandae? Deserunt, voluptas! Saepe provident reprehenderit eius itaque, quis totam, molestiae culpa sit earum distinctio at aperiam?",
+    title: "Title 1",
+    dot: document.querySelector("#dot1"),},
+    {image: "https://th.bing.com/th/id/R.e8e94cfb7eb1113e0b3333f25192b098?rik=fqTHOJ%2br2OiBAw&pid=ImgRaw&r=0",
+    para: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil minima eos similique adipisci recusandae? Deserunt, voluptas! Saepe provident reprehenderit eius itaque, quis totam, molestiae culpa sit earum distinctio at aperiam?",
+    title: "Title 2",
+    dot: document.querySelector("#dot2"),},
+    {image: "https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/H8WuRINimqur8ud/global-economy-finance-background_b86pzpyq__d_thumbnail-1080_01.png",
+    para: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil minima eos similique adipisci recusandae? Deserunt, voluptas! Saepe provident reprehenderit eius itaque, quis totam, molestiae culpa sit earum distinctio at aperiam?",
+    title: "Title 3",
+    dot: document.querySelector("#dot3"),}
+];
+const imgNews = document.querySelector("#newsBox img");
+const paraNews = document.querySelector("#newsBox p");
+const titleNews = document.querySelector("#newsBox h3");
+let newsValue = () => {
+    imgNews.src = newsArr[idx].image;
+    titleNews.textContent = newsArr[idx].title;
+    paraNews.textContent = newsArr[idx].para;
+    newsArr[idx].dot.style.backgroundColor = ("white");
+    idx++;
+};
+let idx = 0;
+
+//AUTOMATED SLIDE NEWS
+newsValue();
+let assignNews = () => {
+    if(idx==newsArr.length){
+        idx=0
+        newsArr[newsArr.length-1].dot.style.backgroundColor = ("transparent");
+        newsValue();
+    }
+    else{
+    newsArr[idx-1].dot.style.backgroundColor = ("transparent");
+    newsValue();
+    }
+};  
+setInterval(assignNews, 8000);
+/* NEWSFEED END */
